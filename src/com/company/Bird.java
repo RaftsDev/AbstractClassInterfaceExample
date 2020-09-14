1089354f3d711319a9f73311d0297a1a74733ee5
Ralph F <RaftsDev@gmail.com>
2020-09-13 22:23:29 -0400

    Feat: Convert Bird class to absctract.

--- src/com/company/Bird.java
+++ src/com/company/Bird.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Bird extends Animal implements CanFly {
+public abstract class Bird extends Animal implements CanFly {
     public Bird(String name) {
         super(name);
     }
